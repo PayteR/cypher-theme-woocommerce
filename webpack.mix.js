0c86bf4958a03b31296084fbfab92518c47eda4e
Peter PayteR Gašparík <payter86@gmail.com>
2019-04-03 08:07:56 +0200

    disabled mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,14 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
-  .sass('scss/_all.scss', 'public/dist')
-  .browserSync({
-    proxy: 'http://127.0.0.1:19061',
-    files: ["public/**/*"],
-    port: 19063,
-    ui: {
-      port: 19064
-    },
-    watch: true
-  });
+// disabled for now
+// mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
+//   .sass('scss/_all.scss', 'public/dist')
+//   .browserSync({
+//     proxy: 'http://127.0.0.1:19061',
+//     files: ["public/**/*"],
+//     port: 19063,
+//     ui: {
+//       port: 19064
+//     },
+//     watch: true
+//   });
